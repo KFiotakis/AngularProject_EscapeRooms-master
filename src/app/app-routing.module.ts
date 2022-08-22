37e7a05d1305a05825492dd1e2852c6b906c1d85
KFiotakis <fiotiskostas@gmail.com>
2022-08-22 15:43:18 +0300

    SPA - Changed Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,9 @@
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component'; 
-import { DetailsComponent } from './components/rooms/details/details.component';
-import { RoomsComponent } from './components/rooms/rooms.component';
-import { AboutComponent } from './components/about/about.component';
-import { BookComponent } from './components/book/book.component';
-import { ContactComponent } from './components/contact/contact.component'; 
-import { SignComponent } from './components/sign/sign.component';  
-
-const routes: Routes = [
-  {path:'',redirectTo:'Home', pathMatch: 'full' },
-  {path:"Home", component:HomeComponent},
-  {path:"Rooms", component:RoomsComponent},
-  {path:"Details", component:DetailsComponent},
-  {path:"About", component:AboutComponent},
-  {path:"Book", component:BookComponent},
-  {path:"Contact", component:ContactComponent},
-  {path:"Sign", component:SignComponent},
-];
+import { RouterModule} from '@angular/router';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule],
   exports: [RouterModule] }
 )
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 
-import {HomeComponent} from './components/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RoomsComponent } from './components/rooms/rooms.component';
 import { DetailsComponent } from './components/rooms/details/details.component';
@@ -17,7 +17,8 @@ import { YouTubePlayerModule } from '@angular/youtube-player';
 import { HomephotosComponent } from './components/home/homephotos/homephotos/homephotos.component';
 import { YoutubeplayerComponent } from './components/home/youtubeplayer/youtubeplayer/youtubeplayer.component';
 import { MapComponent } from './components/home/map/map/map.component';
-import {HttpClientModule} from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,7 @@ import {HttpClientModule} from "@angular/common/http";
     HomephotosComponent,
     YoutubeplayerComponent,
     MapComponent,
-    HomephotosComponent 
+    HomephotosComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +41,23 @@ import {HttpClientModule} from "@angular/common/http";
     AppRoutingModule,
     YouTubePlayerModule,
     SlickCarouselModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot
+      ([
+
+        { path: "Home", component: HomeComponent },
+        { path: "Rooms", component: RoomsComponent },
+        { path: "Details", component: DetailsComponent },
+        { path: "About", component: AboutComponent },
+        { path: "Book", component: BookComponent },
+        { path: "Contact", component: ContactComponent },
+        { path: "Sign", component: SignComponent },
+
+        { path: '', redirectTo: "/Home", pathMatch: 'full' },
+
+
+      ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
