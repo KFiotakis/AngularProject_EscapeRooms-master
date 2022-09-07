e8b6dda2a76403e0351fe88feec7d08409f7e245
ApostolisSn <apostolissn@gmail.com>
2022-09-07 22:16:53 +0300

    Unfinished contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,6 +7,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  contactMethods =[
+    {id:1,name:'Email'},
+    {id:2,name:'SMS'},
+    {id:3,name:'Facebook'},
+    {id:4,name:'Viber'}
+  ];
+
+  log(x:any) {console.log(x);}
+
+  submit(f:any) {console.log(f)}
   
 
   constructor() { }
