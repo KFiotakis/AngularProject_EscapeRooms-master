e07392d20b43e477497f456abdc291c8b2cf8e95
ApostolisSn <apostolissn@gmail.com>
2022-09-12 23:29:09 +0300

    Filtering Rooms

--- src/app/components/rooms/rooms.component.ts
+++ src/app/components/rooms/rooms.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomService } from './room.service';
-import { Room } from './roomsModels';
+import { Room, Genre } from './roomsModels';
 import { IWithActorSettings, WithActorSettings } from './roomSettings';
 import { ICardSettings, CardSettings } from './roomSettings';
 import { ICardImgBodySettings, CardImgBodySettings } from './roomSettings';
+import { NumberValueAccessor } from '@angular/forms';
 
 
 @Component({
@@ -13,6 +14,47 @@ import { ICardImgBodySettings, CardImgBodySettings } from './roomSettings';
 })
 export class RoomsComponent implements OnInit {
 
+  //Filtering
+  searchTitle!: string;
+  searchGenre!: number | string;
+  selectedGenre!: string;
+  FilteredRooms!: Array<Room>;
+  Genres: any[] = [];
+
+  FilterRooms() {
+    this.FilteredRooms = this.Rooms;
+    if (this.searchTitle) {
+      this.FilteredRooms = this.FilteredRooms
+        .filter(x => x.Title.toUpperCase()
+          .includes(this.searchTitle.toUpperCase()))
+    }
+
+    this.GetGenresOption();
+    if (this.searchGenre) {
+      this.FilteredRooms = this.FilteredRooms
+        .filter(x => x.Genre == this.searchGenre)
+
+    }
+  }
+
+  GetGenresOption() {
+     this.searchGenre = this.selectedGenre;
+  }
+
+
+  Method(){
+    for (const key in Genre) {
+      if(!isNaN(parseInt(key))){
+       let temp = {
+         title:Genre[key],
+         no:key
+        }
+        this.Genres.push(temp)
+      }
+  }
+}
+
+  rooms: any;
   dt: any;
   dataDisplay: any;
   Rooms!: Array<Room>;
@@ -20,33 +62,41 @@ export class RoomsComponent implements OnInit {
   CSettings: ICardSettings = CardSettings;
   CImgSettings: ICardImgBodySettings = CardImgBodySettings;
 
-  constructor(private roomService: RoomService) { }
-
-  ngOnInit(): void {
-    this.roomService.getRooms().subscribe(
-      {
-        next: response => {
-          if (response) {
-            hideloader();
-          }
-          this.Rooms = response;
-          this.dt = response;
-          this.dataDisplay = this.dt.data;
-        },
-        error: e => console.log(e),
-        complete: () => console.log(this.Rooms)
-      }
-    );
-    // Function is defined
-    function hideloader() {
-
-      // Setting display of spinner
-      // element to none
-      let ele = document.getElementById('loading');
-      if (ele) {
-        ele.style.display = 'none';
-      }
+  constructor(private roomService: RoomService) {
+      this.Method();
+     
     }
+
+  
+
+
+
+ngOnInit(): void {
+  this.roomService.getRooms().subscribe(
+    {
+      next: response => {
+        if (response) {
+          hideloader();
+        }
+        this.Rooms = response;
+        this.dt = response;
+        this.dataDisplay = this.dt.data;
+        this.FilteredRooms = response;
+      },
+      error: e => console.log(e),
+      complete: () => console.log(this.Rooms)
+    }
+  );
+  // Function is defined
+  function hideloader() {
+
+  // Setting display of spinner
+  // element to none
+  let ele = document.getElementById('loading');
+  if (ele) {
+    ele.style.display = 'none';
+  }
+}
 
   }
 
